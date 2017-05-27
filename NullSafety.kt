033433006b58168034e76a5180b9784355b29b75
Fahim Masud Choudhury <fahim@gobd.co>
2017-05-28 00:38:34 +0600

    Null safety: Elvis operator

--- NullSafety.kt
+++ NullSafety.kt
@@ -19,12 +19,22 @@ fun main(args: Array<String>){
 	val inferredNull = null
 
 	//println(nullMessage.length)		// Compile time error
-	println(nullMessage?.length)		// Elvis operator ?.
+	println(nullMessage?.length)		// ?. Safe call operator
 
 	val serviceProvider = createServiceProvider()
 	serviceProvider?.createService()?.evaluate()
 
 	// Double hashbang operator, I know what I'm doing. 
 	// Let me handle the NPE and don't complain.
-	println(nullMessage!!.length)		
+	//println(nullMessage!!.length)		
+
+	// Elvis operator ?::
+	var mayBeNullValue: String? = "Hello"
+	var isNull = mayBeNullValue?.length ?: true
+	println("Is null? $isNull")
+
+	mayBeNullValue = null
+	isNull = mayBeNullValue?.length ?: true
+	println("Is null? $isNull")
+
 }
